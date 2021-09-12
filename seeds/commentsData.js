0451100bcaf1044b542199eaf3eaa8cd45ac0765
temptado <jason1287712@gmail.com>
2021-09-12 00:13:12 -0400

    added file to seed user table

---
+++ seeds/commentsData.js
@@ -0,0 +1,9 @@
+const { Comment } = require('../models');
+
+const commentsData = [
+    {
+        comment_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed mollis neque, vitae posuere diam. Donec enim ligula, gravida id risus ac, fringilla suscipit ante. Donec vel metus nec eros faucibus facilisis eu ut purus.',
+        user_id: 1,
+        blog_id: 1
+    }
+]
