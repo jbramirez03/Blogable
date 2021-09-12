dc013f105b05539fb7930093cf7d033a5a12e245
temptado <jason1287712@gmail.com>
2021-09-12 18:56:05 -0400

    added configuration to server.js file to contain helper functions in handlebars

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const {User, Blog, Comment} = require('./models');
+const helpers = require('./utils/helpers');
+const path = require('path');
 
 
 const sequelize = require('./config/connection');
@@ -10,6 +12,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -21,15 +25,14 @@ const sess = {
   })
 };
 
-const hbs = exphbs.create({});
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
