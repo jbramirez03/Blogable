e4dfd57ed10e37db03a00c8bc439db3d0e1b91ed
temptado <jason1287712@gmail.com>
2021-09-12 18:36:01 -0400

    updated name of routes for comments and routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
 
-router.use('/user', userRoutes);
-router.use('/comment', commentRoutes);
+router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
