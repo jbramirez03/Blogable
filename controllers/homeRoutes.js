55110957bd264fa971e79f7f2eb99bdea0d6c3c8
temptado <jason1287712@gmail.com>
2021-09-12 15:21:05 -0400

    added include in blogs route to display comments in blog

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,24 @@
 const router = require('express').Router();
-const { Blog, User } = require('../models');
+const { Blog, User, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
         const blogsRawData = await Blog.findAll({
-            include: [{model: User,
-            attributes: ['username']
-            }]
+            include: [{
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['comment_text', 'user_id']
+            }
+            ]
         });
 
         const blogs = blogsRawData.map(blog => blog.get({ plain: true }));
         res.json(blogs);
     } catch (err) {
-        if(err){
+        if (err) {
             res.json(err);
         }
     }
