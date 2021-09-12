de7cf370541803f332f9a68f55a50bc31f8cc22a
temptado <jason1287712@gmail.com>
2021-09-12 18:34:46 -0400

    updated routes to delete comments and add

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,25 +1,40 @@
 const router = require('express').Router();
 const { Comment, User } = require('../../models');
 
-router.get('/:id', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const commentsRawData = await Comment.findAll({
-            where: {
-                blog_id: req.params.id
-            },
-            include: [{
-                model: User,
-                attributes: ['username']
-            }]
+        const newComment = await Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            blog_id: req.body.blog_id
         });
 
-        const comments = commentsRawData.map(comment => comment.get({plain: true}));
-        res.json(comments);
+        res.status(200).json(newComment);
     } catch (err) {
-        if(err){
-            res.json(err);
-        }
+        res.status(400).json(err);
     }
 });
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id.' });
+            return;
+        }
+
+        res.status(400).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
