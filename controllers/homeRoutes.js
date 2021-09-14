98672f4f3bc9690e7784ec8d747bcdf977b2d1dd
temptado <jason1287712@gmail.com>
2021-09-13 20:37:31 -0400

    latest update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -60,6 +60,16 @@ router.get('/blog/:id', async (req, res) => {
     }
 });
 
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('homepage');
+      return;
+    }
+  
+    res.render('login');
+  });
+
 
 
 module.exports = router;
