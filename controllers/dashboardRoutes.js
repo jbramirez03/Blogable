c43e49f19a71d9b227fd7481aa0a6e926da8a444
temptado <jason1287712@gmail.com>
2021-09-17 21:42:58 -0400

    created first dashboard route to get all of a users posts

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,3 +1,32 @@
 const router = require('express').Router();
 const { Blog, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const userBlogs = Blog.findAll({
+            where: { user_id: req.session.user_id },
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['comment_text', 'date_created'],
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['username']
+                        }
+                    ]
+                }
+            ]
+        });
+
+        const blogs = (await userBlogs).map(blog => blog.get({ plain: true }));
+        console.log(blogs);
+        res.json(blogs);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homepageRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/', homepageRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
