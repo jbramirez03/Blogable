8fc37f4f1f1df3ce60859d737e14a90b29949bde
temptado <jason1287712@gmail.com>
2021-10-02 19:13:26 -0400

    created route to edit blog

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -17,6 +17,32 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+      const blogData = await Blog.update(
+        {
+          blog_name: req.body.blog_name,  
+          description: req.body.description,
+        },
+        {
+          where: {
+            id: req.params.id,
+          },
+        },
+      );
+  
+      if (!blogData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(blogData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+  
+
 router.delete('/', withAuth, async (req, res) => {
     try {
         const blogData = await Blog.destroy({
